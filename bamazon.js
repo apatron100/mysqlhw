264a871edb0302912cd08c2acce8c4702d59510e
alfredo patron <apatron100@cox.net>
2017-11-22 21:45:01 -0800

    third commiment

--- bamazon.js
+++ bamazon.js
@@ -21,6 +21,53 @@ var makeTable = function(){
 
 
 				}
-		}
-	})
+		}promtCustomer = function(res){
+			inquire.promt([{
+				type:'input',
+				name:'choice',
+				message "What do you want to buy? [Salir con Q]"
+			}]).then(function(answer){
+				var correct = false;
+				for(var i=0;i<res.length;i++){
+					if(res[i].productname==answer.choice;
+						correct = true;
+						var product=answer.choice;
+						var id=i;
+						inquirer.promt({
+							type:"input",
+							name:"quant",
+							message:"how many would you like to buy",
+							validate: function(value){
+								if(isNaN(value)==false){
+									return true;
+								} else { 
+									return false;		
+								}	
+							}
+						}).then(function(answer){
+							if((res[id].stockquantity-answer.quant)>0){
+								connection.query("UPDATE products SET
+									stockquantity='"+(res[id].stockquantity-
+									answer.quant)+"' WHERE productname='"+product
+								+"'", function(err,res2) {
+									console.log("product purchased!");
+									makeTable();
+								})
+
+						} else {
+							console.log("Seleccion no valida")
+							promtCustomer(res);
+
+						}
+
+						})
+				}
+			}
+						if(i==res.length && correct==false){
+							console.log("seleccion no valida");
+							promtCustomer(res);
+						}
+
+			})
+	}	}
 }
